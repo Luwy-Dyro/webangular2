69d77518932c6cf709a7284cd90d6d71b843a587
Luwypack <etx.webdesigner@gmail.com>
2017-10-07 14:47:29 -0500

    Header, Body, Footer Listo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,18 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { BodyComponent } from './components/body/body.component';
+import { PortafolioComponent } from './components/portafolio/portafolio.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    BodyComponent,
+    PortafolioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
