592021d1c4e7b629853611c61bd5b809c8f2bbdc
Luwypack <etx.webdesigner@gmail.com>
2017-10-09 15:29:37 -0500

    Se agrego el loading animado

--- src/app/services/productos.service.ts
+++ src/app/services/productos.service.ts
@@ -4,7 +4,7 @@ import { Http } from '@angular/http';
 @Injectable()
 export class ProductosService {
   productos:any[]=[];
-  cargando:boolean;
+  cargando:boolean = true
 
   constructor(private http:Http) {
       this.cargar_productos();
@@ -15,10 +15,11 @@ export class ProductosService {
     this.cargando = true;
     this.http.get('https://website-82e16.firebaseio.com/productos_idx.json')
               .subscribe(res => {
-
                 console.log(res.json());
-                this.cargando = false;
-                this.productos = res.json();
+                setTimeout( ()=> {
+                  this.cargando = false;
+                  this.productos = res.json();
+                }, 1500);
               });
   }
 
